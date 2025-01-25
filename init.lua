70c7b9364c9e6a234610fb67c419ce57b75457b4
H0meV@LinuxServer <admin@fkj.cn>
2025-01-25 08:22:42 +0800

    update

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require("plugins.plugins-setup")
 require("plugins.lualine")
 require("plugins.nvim-tree")
+require("plugins.treesitter")
 
 require("core.options")
 require("core.keymaps")
--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -1,55 +1,26 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+local plugins = {
+  "folke/tokyonight.nvim", -- 主题
+  "nvim-lualine/lualine.nvim",  -- 状态栏
+  "nvim-tree/nvim-tree.lua",  -- 文档树
+  "nvim-tree/nvim-web-devicons", -- 文档树图标
 
--- 保存此文件自动更新安装软件
--- 注意PackerCompile改成了PackerSync
--- plugins.lua改成了plugins-setup.lua，适应本地文件名字
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
-  augroup end
-]])
+  "christoomey/vim-tmux-navigator", -- 用ctl-hjkl来定位窗口
+  "nvim-treesitter/nvim-treesitter", -- 语法高亮
+  "p00f/nvim-ts-rainbow", -- 配合treesitter，不同括号颜色区分
+}
 
-return require('packer').startup(function(use)
-  -- packer 主体
-  use 'wbthomason/packer.nvim'
-
-  -- tokyo 主题
-  use 'folke/tokyonight.nvim'
-
-  -- 底部状态栏 + ICON
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-
-  -- 文档树 + ICON
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons',
-    }
-  }
-
-  -- 用ctl-hjkl来定位窗口
-  use "christoomey/vim-tmux-navigator"
-
-  -- 语法高亮 + 彩虹括号
-  use "nvim-treesitter/nvim-treesitter"
-  use "p00f/nvim-ts-rainbow"
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+local opts = {} -- 注意要定义这个变量
+require("lazy").setup(plugins, opts)
